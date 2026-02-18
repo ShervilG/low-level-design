a988bb785a1d3a9ce18440bd0baef1a378f89a64
ShervilG <wintergpt45@gmail.com>
2026-02-18 11:38:57 +0530

    some changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,40 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
+    public static boolean hasAlternatingBits(int n) {
+        if (n <= 2)
+            return true;
+
+        int setBitPos = 0;
+        int temp = n;
+        int counter = 0;
+        while (temp != 0) {
+            if ((temp & 1) == 1) {
+                setBitPos = counter;
+            }
+
+            temp = temp >> 1;
+            counter += 1;
+        }
+
+        int currentBit = n & 1;
+        n = n >> 1;
+        setBitPos--;
+        while (setBitPos >= 0) {
+            if (currentBit == (n & 1)) {
+                return false;
+            }
+
+            currentBit = n & 1;
+            n = n >> 1;
+            setBitPos--;
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println(hasAlternatingBits(5));
     }
 }
